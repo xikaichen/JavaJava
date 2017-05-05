8df45d557bed219b166357fbf24aacdc63e24a8e
xikaichen <xc2358@columbia.edu>
2017-05-04 20:17:15 -0400

    randomly brush leetcode

---
+++ LeetCodeLeetCode/344. Reverse String.java
@@ -0,0 +1,15 @@
+public class Solution {
+    public String reverseString(String s) {
+        char[] chars = s.toCharArray();
+        int i = 0;
+        int j = chars.length - 1;
+        while (i < j) {
+            char temp = chars[i];
+            chars[i] = chars[j];
+            chars[j] = temp;
+            i++;
+            j--;
+        }
+        return new String(chars);  //just know char[] to string, use new String(chars)
+    }
+}
