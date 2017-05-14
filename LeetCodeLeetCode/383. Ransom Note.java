91817086862057449787b3772868a31641803a74
xikaichen <xc2358@columbia.edu>
2017-05-14 16:17:19 -0400

    randomly brushing leetcode

---
+++ LeetCodeLeetCode/383. Ransom Note.java
@@ -0,0 +1,18 @@
+//order doesn't matter, "aba" can construct "aa"
+public class Solution {
+    public boolean canConstruct(String ransomNote, String magazine) {
+        int[] map = new int[26];
+        for (int i = 0; i < magazine.length(); i++) {
+            map[magazine.charAt(i) - 'a']++;
+        }
+        for (int j = 0; j < ransomNote.length(); j++) {
+            map[ransomNote.charAt(j) - 'a']--;
+        }
+        for (int k = 0; k < map.length; k++) {
+            if (map[k] < 0) {
+                return false;
+            }
+        }
+        return true;
+    }
+}
---
+++ LeetCodeLeetCode/404. Sum of Left Leaves.java
@@ -0,0 +1,22 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode(int x) { val = x; }
+ * }
+ */
+public class Solution {
+    public int sumOfLeftLeaves(TreeNode root) {
+        if (root == null) {
+            return 0;
+        }
+        if (root.left != null && root.left.left == null && root.left.right == null) { // look at the left node, if it satisfied the condition, look at the right node.
+            return root.left.val + sumOfLeftLeaves(root.right);
+        }
+        return sumOfLeftLeaves(root.left) + sumOfLeftLeaves(root.right); // final result = the result of left + the result of right
+
+    }
+}
+
