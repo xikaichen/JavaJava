b55b8616ac4ceb53937d7912219be9a102b67695
xikaichen <xc2358@columbia.edu>
2017-05-15 19:17:47 -0400

    randomly brushing leetcode

---
+++ LeetCodeLeetCode/198. House Robber.java
@@ -0,0 +1,13 @@
+public class Solution {
+    public int rob(int[] nums) {
+        int[][] dp = new int[nums.length + 1][2];  // two dimension dp, the second dimension is the states
+        for (int i = 1; i <= nums.length; i++) {
+            dp[i][0] = Math.max(dp[i - 1][0], dp[i - 1][1]);
+            dp[i][1] = nums[i - 1] + dp[i - 1][0];
+        }
+        return Math.max(dp[nums.length][0], dp[nums.length][1]);
+    }
+}
+
+
+
