ae71b19c8b63b32039e523a83b262ed522a53d20
xikaichen <xc2358@columbia.edu>
2017-05-15 20:41:44 -0400

    randomly brushing leetcode

---
+++ LeetCodeLeetCode/119. Pascal's Triangle II.java
@@ -0,0 +1,17 @@
+public class Solution {
+    public List<Integer> getRow(int rowIndex) {
+        List<Integer> list = new LinkedList<Integer>();
+        for (int i = 0; i < rowIndex; i++) {
+            list.add(0);
+        }
+        list.add(1);
+        for (int i = rowIndex - 1; i >= 0; i--) { // update from tail to head
+            for (int j = i; j < rowIndex; j++) { // add from current to tail
+                list.set(j, list.get(j + 1) + list.get(j));
+            }
+        }
+        return list;
+    }
+}
+
+
