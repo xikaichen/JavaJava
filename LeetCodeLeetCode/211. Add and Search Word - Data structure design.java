aa2a3cf2b374cebe3793d351f581c236154c59b2
xikaichen <xc2358@columbia.edu>
2017-06-23 20:04:56 +0800

    randomly brushing leetcode

--- LeetCodeLeetCode/211. Add and Search Word - Data structure design.java
+++ LeetCodeLeetCode/211. Add and Search Word - Data structure design.java
@@ -1,4 +1,6 @@
+//Trie 树
 public class WordDictionary {
+    //第一步：定义TrieNode类
     public class TrieNode {
         public TrieNode[] children = new TrieNode[26];
         public String item = "";
@@ -8,18 +10,19 @@ public class WordDictionary {
 
     
     /** Adds a word into the data structure. */
+    //加word进Tree
     public void addWord(String word) {
         TrieNode node = root;
         if (word == null) {
             return;
         }
         for (char c : word.toCharArray()) {
-            if (node.children[c - 'a'] == null) {
+            if (node.children[c - 'a'] == null) { // 遍历word，如果出现这个字母就new一个TrieNode节点
                 node.children[c - 'a'] = new TrieNode();
             }
-            node = node.children[c - 'a'];
+            node = node.children[c - 'a']; // node转到下一个
         }
-        node.item = word;
+        node.item = word; // 最后一个点 才给item赋值
     }
     
     /** Returns if the word is in the data structure. A word could contain the dot character '.' to represent any one letter. */
@@ -28,13 +31,13 @@ public class WordDictionary {
     }
     
     private boolean match(TrieNode root, String word, int idx) {
-        if (idx == word.length()) {
+        if (idx == word.length()) { 
             return !root.item.equals("");
         }
         if (word.charAt(idx) == '.') {
             for (int i = 0; i < 26; i++) {
                 if (root.children[i] != null) {
-                    if (match(root.children[i], word, idx + 1)) {
+                    if (match(root.children[i], word, idx + 1)) { // recursion
                         return true;
                     }
                 }
