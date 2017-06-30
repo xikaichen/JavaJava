41c2c308221abe0224f42c7c5717a27868557712
xikaichen <xc2358@columbia.edu>
2017-06-30 18:39:51 +0800

    randomly brushing leetcode

---
+++ LeetCodeLeetCode/204. Count Primes.java
@@ -0,0 +1,19 @@
+
+//use sieve algorithm
+//boolean[] initial is all false
+//1 is not prime, so start from 2
+public class Solution {
+    public int countPrimes(int n) {
+        boolean[] notPrime = new boolean[n];
+        int count = 0;
+        for (int i = 2; i < n; i++) { // as the question's requirement find prime less than n, so not include n
+            if (notPrime[i] == false) {
+                count++;
+                for (int j = 2; i * j < n; j++) {
+                    notPrime[i * j] = true;
+                }
+            }
+        }
+        return count;
+    }
+}
