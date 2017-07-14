fd6fb2c980197674303df9d3d7930b3d72100bee
xikaichen <xc2358@columbia.edu>
2017-07-14 16:23:40 +0800

    Trie

--- NotRandomNotRandom/Trie/208. Implement Trie (Prefix Tree).java
+++ NotRandomNotRandom/Trie/208. Implement Trie (Prefix Tree).java
@@ -48,6 +48,61 @@ public class Trie {
     }
 }
 
+
+// 非recursion的写法
+/**
+ * Your Trie object will be instantiated and called as such:
+ * Trie obj = new Trie();
+ * obj.insert(word);
+ * boolean param_2 = obj.search(word);
+ * boolean param_3 = obj.startsWith(prefix);
+ */
+public class Trie {
+    public class TrieNode {
+        TrieNode[] children = new TrieNode[26];
+        String item = "";
+    }
+
+    /** Initialize your data structure here. */
+    private TrieNode root = new TrieNode();
+    
+    /** Inserts a word into the trie. */
+    public void insert(String word) {
+        TrieNode node = root;
+        for (char c : word.toCharArray()) {
+            if (node.children[c - 'a'] == null) {
+                node.children[c - 'a'] = new TrieNode();
+            }
+            node = node.children[c - 'a'];
+        }
+        node.item = word;
+    }
+    
+    /** Returns if the word is in the trie. */
+    public boolean search(String word) {
+        TrieNode node = root;
+        for (char c : word.toCharArray()) {
+            if (node.children[c - 'a'] == null) {
+                return false;
+            }
+            node = node.children[c - 'a'];
+        }
+        return !node.item.equals("");
+    }
+    
+    /** Returns if there is any word in the trie that starts with the given prefix. */
+    public boolean startsWith(String prefix) {
+        TrieNode node = root;
+        for (char c : prefix.toCharArray()) {
+            if (node.children[c - 'a'] == null) {
+                return false;
+            }
+            node = node.children[c - 'a'];
+        }
+        return true;
+    }
+}
+
 /**
  * Your Trie object will be instantiated and called as such:
  * Trie obj = new Trie();
