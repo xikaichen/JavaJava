9868b4d69e84862d1908debf94d030d313dec505
xikaichen <xc2358@columbia.edu>
2017-08-09 18:08:20 -0400

    sort algorithm

--- LintCodeLintCode/SortAlgorithm/SortAlgorithm.java
+++ LintCodeLintCode/SortAlgorithm/SortAlgorithm.java
@@ -86,6 +86,23 @@ private void merge(int[] nums, int start, int mid, int end, int[] temp) {
 	}
 }
 
+3. insertion sort
+
+public void insertion(int[] nums) {
+	if (nums.length <= 1) {
+		return;
+	}
+	for (int i = 0;i < nums.length; i++) {
+		int key = nums[i];
+		int j = i - 1;
+		while (j >= 0 && nums[j] > key) {
+			nums[j + 1] = nums[j];
+			j--;
+		}
+		nums[j + 1] = key;
+	}
+}
+
 
 
 
