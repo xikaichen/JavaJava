08c2ca803a0dcbb1e10f76c516019994db04f950
xikaichen <xc2358@columbia.edu>
2017-08-12 16:03:13 -0400

    array

--- NotRandomNotRandom/Array/219. Contains Duplicate II.java
+++ NotRandomNotRandom/Array/219. Contains Duplicate II.java
@@ -3,7 +3,7 @@ public class Solution {
         if (nums == null) {
             return false;
         }
-        HashMap<Integer, Integer> map = new HashMap<Integer, Integer>();
+        Map<Integer, Integer> map = new HashMap<Integer, Integer>();
         for (int i = 0; i < nums.length; i++) {
             if (map.containsKey(nums[i]) && Math.abs(map.get(nums[i]) - i) <= k) {
                 return true;
--- NotRandomNotRandom/Array/88. Merge Sorted Array.java
+++ NotRandomNotRandom/Array/88. Merge Sorted Array.java
@@ -1,7 +1,7 @@
 public class Solution {
     public void merge(int[] nums1, int m, int[] nums2, int n) {
         int i = m + n - 1;
-        while (i >= 0 && n > 0 && m > 0) {
+        while (n > 0 && m > 0) {
             if (nums2[n - 1] > nums1[m - 1]) {
                 nums1[i] = nums2[n - 1];
                 n--;
