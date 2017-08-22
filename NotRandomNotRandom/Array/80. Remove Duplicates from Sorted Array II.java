5d44d9eb3b63cd55992b628cb2ce07641582ff21
xikaichen <xc2358@columbia.edu>
2017-08-22 11:33:30 -0400

    array

--- NotRandomNotRandom/Array/80. Remove Duplicates from Sorted Array II.java
+++ NotRandomNotRandom/Array/80. Remove Duplicates from Sorted Array II.java
@@ -2,8 +2,9 @@ public class Solution {
     public int removeDuplicates(int[] nums) {
         int i = 0;
         for (int n : nums)
-            if (i < 2 || n > nums[i - 2]) // use i - 2 to store at least two duplicates
+            if (i < 2 || n != nums[i - 2]) { // use i - 2 to store at least two duplicates
                 nums[i++] = n;
+            }
         return i;
     }
 }
